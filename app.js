64edd377e577224a2d30073033650021713fb8f2
Furqi123 <124422693+Furqi123@users.noreply.github.com>
2023-11-28 05:16:40 -0800

    code update

--- app.js
+++ app.js
@@ -1,7 +1,74 @@
-const obj = {
-    
-}
+// const obj = {
+//   bodyMassageOil : {
+//     relaxation: "Promotes relaxation and reduces stress.",
+//     flexibility: "Enhances flexibility and joint mobility.",
+//     painRelief: "Provides relief from pain and soreness.",
+//     sleepAid: "Facilitates better sleep and rest.",
 
-function search(){
+//   }  ,
+//   babyHairOil: {
+//     softness: "Promotes soft and manageable baby hair.",
+//     soothing: "Soothes and calms the baby's scalp, reducing dryness.",
+//     hairGrowth: "Supports healthy hair growth in babies.",
+//     naturalIngredients: "Made with natural ingredients suitable for baby's skin.",
 
-}
+//   },
+//   babyMassageOil: {
+//     skinHydration: "Promotes skin hydration, keeping the baby's skin soft.",
+//   relaxation: "Aids in relaxation, helping the baby sleep more peacefully.",
+//   bonding: "Enhances parent-baby bonding during massage sessions.",
+//   digestionAid: "May support digestion and relieve tummy discomfort in babies."
+//   },
+//   aloeVeraOil:{
+//     skinMoisturization: "Provides intense moisturization for the skin.",
+//     soothingEffect: "Soothes and calms irritated or sunburned skin.",
+//     antiInflammatory: "Possesses anti-inflammatory properties, reducing redness and swelling.",
+//     hairCare: "Nourishes and conditions the hair, promoting a healthy scalp."
+//   },
+//   blackSeedOil:{
+//     immuneSupport: "Supports and boosts the immune system.",
+//   antiInflammatory: "Possesses anti-inflammatory properties, aiding in reducing inflammation.",
+//   skinHealth: "Promotes healthy skin and may alleviate certain skin conditions.",
+//   respiratoryHealth: "May contribute to respiratory health and alleviate symptoms of asthma."
+//   },
+//   sweetAlmondOil:{
+//     skinNourishment: "Nourishes and moisturizes the skin, keeping it soft and supple.",
+//   hairCare: "Conditions and strengthens hair, promoting shine and reducing breakage.",
+//   antiAging: "Contains antioxidants that may help reduce signs of aging on the skin.",
+//   darkCircleReduction: "May contribute to reducing dark circles and puffiness around the eyes."
+//   },
+//   bitterAlmondOil:{
+//     aromaTherapy: "Used in aromatherapy for its pleasant and calming fragrance.",
+//     skinCleansing: "Helps cleanse and purify the skin, removing impurities.",
+//     muscleRelaxation: "May contribute to muscle relaxation when used in massage.",
+//     scalpHealth: "Promotes a healthy scalp and may alleviate certain scalp conditions."
+//   },
+//   oliveOil:{
+//     heartHealth: "Promotes heart health by reducing the risk of cardiovascular diseases.",
+//     antiInflammatory: "Contains anti-inflammatory properties that may reduce inflammation.",
+//     skinCare: "Nourishes and moisturizes the skin, keeping it healthy and radiant.",
+//     culinaryVersatility: "Versatile for cooking, enhancing flavors in various cuisines."
+//   },
+//   fiveOil:{
+//     immuneSupport: "Supports the immune system with a blend of essential oils.",
+//   stressRelief: "Promotes relaxation and helps alleviate stress and tension.",
+//   skinHealth: "Nourishes the skin, leaving it moisturized and radiant.",
+//   mentalClarity: "Aids in mental clarity and concentration with aromatic properties."
+//   },
+//   tenOil:{
+//     holisticWellness: "Promotes holistic wellness with a blend of ten essential oils.",
+//     moodElevation: "Aids in mood elevation and emotional balance.",
+//     hairNourishment: "Nourishes and conditions the hair, promoting a healthy scalp.",
+//     respiratorySupport: "Provides respiratory support for easier breathing."
+//   }
+// }
+
+
+// function search(){
+
+// }
+
+
+// var userColor = prompt("enter color")//brown
+// var index =prompt("enter index") //1
+// var color = ["black","green","yellow"]
